83fba1dbdbe28b8c53a56d860c7757adbb23d010
Tomek <137789823+TomekRadzioch@users.noreply.github.com>
2023-09-06 14:51:28 +0200

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,9 +1,1 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
